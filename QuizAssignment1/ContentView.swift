929391d2cc2bfcce6a865dd1a759eff53381ab64
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-16 23:07:10 +0100

    Initial Commit

---
+++ QuizAssignment1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizAssignment1
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 16.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuizAssignment1/QuizAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAssignment1App.swift
+//  QuizAssignment1
+//
+//  Created by Prof. Dr. Nunkesser, Robin on 16.12.20.
+//
+
+import SwiftUI
+
+@main
+struct QuizAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
